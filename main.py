c1e092aa878ed23f5ca4181638be3bccbe6c3e73
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-11-24 17:19:31 -0500

    pandas

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import os
 import csv
+import pandas as pd
+
 os.chdir("Datos")
+
 ciudades = set()
 Grafo = [[None] * 49] * 49
-with open("Datos vias Colombia.csv") as f:
-    lector = csv.reader(f)
-    for row in lector:
-        ciudades.add(row[0])
-        ciudades.add(row[1])
-    ciudades.remove('')
-    ciudades = sorted(list(ciudades))
-    for row in lector:
-        if row[0] in ciudades:
-            arista = (row[2], row[3])
-            Grafo[ciudades.index(row[0])][ciudades.index(row[1])] = arista
-   
-print(ciudades)         
-print(Grafo)
 
+datos = pd.read_csv("Datos vias Colombia.csv")
+ciudades.update(datos["A"].tolist() + datos["B"].tolist())
+print(ciudades)
+ 
     
 
 def carretera_unica(a,b):
