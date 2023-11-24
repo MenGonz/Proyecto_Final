cc21895d1f2d196f798657378e167d445f7ebc87
NicolasOrozcoMedina <norozcom@unal.edu.co>
2023-11-24 17:44:49 -0500

    Update main.py
    a

--- main.py
+++ main.py
@@ -11,18 +11,30 @@ datos = pd.read_csv("Datos vias Colombia.csv")
 ciudades.update(datos["A"].tolist() + datos["B"].tolist())
 print(ciudades)
  
-    
 
-def carretera_unica(a,b):
-    comprobante=True
+
+
+
+
+
+
+def carretera_unica(matriz,a,b):
+    comprobante = True
     return comprobante
 
-def ruta_corta_kilometros(a,b):
-    ruta=None #indice 0 de la tupla
+def Dijkstra(matriz,a,b,indice):
+    #Camino mas corto por el indice que se especifico 
+    ruta = None 
     return ruta
 
-def ruta_corta_minutos(a,b):
-    ruta=None #indice 1 de la tupla
+def ruta_corta_kilometros(matriz,a,b):
+    indice = 0
+    ruta = Dijkstra(matriz,a,b,indice) #indice 0 de la tupla
+    return ruta 
+
+def ruta_corta_minutos(matriz,a,b):
+    indice = 1
+    ruta = Dijkstra(matriz,a,b,indice) #indice 1 de la tupla
     return ruta
 
 
