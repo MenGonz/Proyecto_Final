cc4a37be0e40f616fa540e91e5564f69446dc7e0
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-11-24 18:16:05 -0500

    commit

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ datos = pd.read_csv("Datos vias Colombia.csv")
 ciudad.update(datos["A"].tolist() + datos["B"].tolist())
 ciudades = sorted(list(ciudad))
 for i in range(len(datos)):
+    #print(datos.iloc[i]["A"], datos.iloc[i]["B"], datos.iloc[i]["KM"], datos.iloc[i]["Minutos"])
     Grafo[ciudades.index(datos.iloc[i]["A"])][ciudades.index(datos.iloc[i]["B"])] = (datos.iloc[i]["KM"], datos.iloc[i]["Minutos"])
     Grafo[ciudades.index(datos.iloc[i]["B"])][ciudades.index(datos.iloc[i]["A"])] = (datos.iloc[i]["KM"], datos.iloc[i]["Minutos"])
 
-print(Grafo)
+
 
 def carretera_unica(a,b):
     if Grafo[ciudades.index(a)][ciudades.index(b)] == None:
         comprobante = "No hay ninguna via entre las ciudades"
     else:
-        comprobante = "La via entre " + a + " y " + b + " tiene " + Grafo[ciudades.index(a)][ciudades.index(b)][0] +  " kilometros y un tiempo de recorrido de " + Grafo[ciudades.index(a)][ciudades.index(b)][1] + " minutos" 
+        comprobante = "La via entre " + a + " y " + b + " tiene " + str(Grafo[ciudades.index(a)][ciudades.index(b)][0]) +  " kilometros y un tiempo de recorrido de " + str(Grafo[ciudades.index(a)][ciudades.index(b)][1]) + " minutos" 
     return comprobante
 
 def Dijkstra(matriz,a,b,indice):
@@ -38,4 +39,4 @@ def ruta_corta_minutos(matriz,a,b):
     ruta = Dijkstra(matriz,a,b,indice) #indice 1 de la tupla
     return ruta
 
-
+print(carretera_unica("Medellin", "Pereira"))
