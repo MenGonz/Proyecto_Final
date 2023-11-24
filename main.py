4fa139882f8e112e9ac8ac38ddcc32776f871ddd
NicolasOrozcoMedina <norozcom@unal.edu.co>
2023-11-24 16:43:02 -0500

    Update main.py
    funciones

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ os.chdir("Datos")
 ciudades = set()
 with open("Datos vias Colombia.csv") as f:
     lector = csv.reader(f)
-    
+
+def carretera_unica(a,b):
+    comprobante=True
+    return comprobante
+
+def ruta_corta_kilometros(a,b):
+    ruta=None #indice 0 de la tupla
+    return ruta
+
+def ruta_corta_minutos(a,b):
+    ruta=None #indice 1 de la tupla
+    return ruta
+
