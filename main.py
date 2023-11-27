ce6de7ea6f2e44665ae169230ba566ece6460f70
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-11-27 14:15:19 -0500

    dijkstra

--- main.py
+++ main.py
@@ -21,9 +21,60 @@ def carretera_unica(a,b):
         comprobante = "La via entre " + a + " y " + b + " tiene " + str(Grafo[ciudades.index(a)][ciudades.index(b)][0]) +  " kilometros y un tiempo de recorrido de " + str(Grafo[ciudades.index(a)][ciudades.index(b)][1]) + " minutos" 
     return comprobante
 
+def dijkstra(M, inicial, terminal): #definimos el algoritmo dijkstra como una función
+    """
+    Este algoritmo recibe una matriz de pesos M y un nodo inicial r, y devuelve
+    una lista de predecesores y una lista de pesos, que representan una r-ramificación de
+    caminos mínimos desde r a cada vértice del grafo.
+    """
+    
+    # Definimos lo que usaremos:
+    n = len(M)                          #n es la cantidad de vértices del grafo
+    p: list[int] = [None]*n             #lista que contendrá la información de los predecesores
+    l: list[int] = [float('inf')]*n     #lista que contendrá la información de los pesos
+    l[inicial] = 0                            #peso del nodo inicial es 0
+    p[inicial] = inicial                            #predecesor del nodo inicial es él mismo
+    coloreado: list[bool] = [False]*n   #coloreamos un vértice cuando lo visitamos, inicialmente están sin colorear
+    
+    
+    while False in coloreado:           #Mientras hayan vértices sin colorear (es decir, sin visitar)
+        
+        nocoloreados = [v for v in range(n) if not coloreado[v]]    #Los tomo
+        u = min(nocoloreados, key=lambda i: l[i])                   #saco el de menor peso
+        coloreado[u] = True                                         #lo coloreo
+        
+        for v in range(n):                                          #para cada vértice adyacente a u
+            #Reviso si el peso de v es mayor que el peso de u mas el peso de la arista (u,v)
+            if M[u][v] > 0 and l[v] > l[u] + M[u][v]:      
+                #en caso de que sí, actualizo el peso de v 
+                l[v] = l[u] + M[u][v]
+                #y actualizo también su predecesor
+                p[v] = u
+            
+    camino = []
+    camino.append(terminal)
+    t = terminal
+    while p[t] != inicial:
+        t = p[t]
+        camino.append(p[t])
+        
+        
+    camino.reverse()
+    
+    return camino,l
 def Dijkstra(Grafo,a,b,indice):
-    #Camino mas corto por el indice que se especifico 
-    ruta = None 
+    #Camino mas corto por el indice que se especifico
+    #print(Grafo) 
+    Grafo1 = [[0 for i in range(48)] for j in range(48)]
+    for i in range(48):
+        for j in range(48):
+            if Grafo[i][j] != 0:
+                Grafo1[i][j] = Grafo[i][j][indice]
+            
+    camino, l = dijkstra(Grafo1, ciudades.index(a), ciudades.index(b))
+    
+    
+    ruta = list(map(lambda i: ciudades[i],camino ))
     return ruta
 
 def ruta_corta_kilometros(Grafo,a,b):
